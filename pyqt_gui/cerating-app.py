6ef0f84f21bbf40eae61a30a612da1a497db277d
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-06-02 14:05:55 +0300

    Add Icon

--- pyqt_gui/cerating-app.py
+++ pyqt_gui/cerating-app.py
@@ -1,10 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 def window(): # create window
     app = QApplication(sys.argv)
     win = QMainWindow()
+
+    win.setWindowTitle('Python Simple GUI')
+    win.setGeometry(100,100,750,750)
+    win.setWindowIcon(QIcon('index2.png'))
+
 
     win.show()
     sys.exit(app.exec_()) 
