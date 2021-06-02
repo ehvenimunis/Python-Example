d3ce161c11d382334a34ebdf6416aa99dfb1fac8
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-06-02 20:33:30 +0300

    Add msg box

--- pyqt_gui/_msgbox.py
+++ pyqt_gui/_msgbox.py
@@ -3,18 +3,20 @@ from PyQt5.QtWidgets import QMessageBox
 from _msgboxForm import Ui_MainWindow
 import sys
 
+
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Window, self).__init__()   
+        super(Window, self).__init__()
 
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)  
+        self.ui.setupUi(self)
 
-        self.ui.btnExit.clicked.connect(self.showDialog)
+        self.ui.btnExit.clicked.connect(self.showDialog)  # butona ulaş
 
     def showDialog(self):
 
-        result = QMessageBox.question(self, 'Close Application', 'Are you sure ?', QMessageBox.Ok | QMessageBox.Cancel | QMessageBox.Ignore, QMessageBox.Cancel)
+        result = QMessageBox.question(self, 'Close Application', 'Are you sure ?',
+                                      QMessageBox.Ok | QMessageBox.Cancel | QMessageBox.Ignore, QMessageBox.Cancel)
         if result == QMessageBox.Ok:
             print('Yes clicked')
             QtWidgets.qApp.quit()
@@ -44,12 +46,13 @@ class Window(QtWidgets.QMainWindow):
     #         print('Cancel...')
     #     else:
     #         print('Ignore...')
+
+
 def app():
     app = QtWidgets.QApplication(sys.argv)
     win = Window()
     win.show()
     sys.exit(app.exec_())
 
+
 app()
-
-
