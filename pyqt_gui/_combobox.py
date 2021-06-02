46849819832f8f730d14e0e388dfb497b5f2f1f8
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-06-02 20:21:28 +0300

    Add combobox

--- pyqt_gui/_combobox.py
+++ pyqt_gui/_combobox.py
@@ -2,15 +2,17 @@ from PyQt5 import QtWidgets
 import sys
 from _comboboxForm import Ui_MainWindow
 
+
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-    
+
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)      
+        self.ui.setupUi(self)
 
         combo = self.ui.cbSehirler
 
+        # combobox içerisine eleman ekleme
         # combo.addItem('Ankara')
         # combo.addItem('İstanbul')
         # combo.addItem('Kocaeli')
@@ -20,21 +22,23 @@ class Window(QtWidgets.QMainWindow):
         self.ui.btnGetItem.clicked.connect(self.GetItem)
         self.ui.btnClear.clicked.connect(self.ClearItems)
 
-        self.ui.cbSehirler.currentIndexChanged.connect(self.SelectedChangedIndex)
-        self.ui.cbSehirler.currentIndexChanged[str].connect(self.SelectedChangedText)
+        self.ui.cbSehirler.currentIndexChanged.connect(
+            self.SelectedChangedIndex)
+        self.ui.cbSehirler.currentIndexChanged[str].connect(
+            self.SelectedChangedText)
 
     def ClearItems(self):
         self.ui.cbSehirler.clear()
 
     def LoadItems(self):
-        sehirler = ['Adana','İzmir','Rize']
-        
+        sehirler = ['Adana', 'İzmir', 'Rize']  # elemanları yükler
+
         self.ui.cbSehirler.addItems(sehirler)
 
-    def GetItem(self):
+    def GetItem(self):  # seçili olan eleman için tex bilgisi verir
         print(self.ui.cbSehirler.currentText())
         print(self.ui.cbSehirler.currentIndex())
-        
+
         count = self.ui.cbSehirler.count()
         for index in range(count):
             print(self.ui.cbSehirler.itemText(index))
@@ -44,7 +48,8 @@ class Window(QtWidgets.QMainWindow):
 
     def SelectedChangedText(self, text):
         print(text)
-   
+
+
 app = QtWidgets.QApplication(sys.argv)
 win = Window()
 win.show()
